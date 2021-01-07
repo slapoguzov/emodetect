9178ffd947fb04859ba0397c13eb6c6c249cccca
mda031094 <76900108+mda031094@users.noreply.github.com>
2021-01-07 21:55:50 +0300

    Update ClassExtensions.kt

--- core/src/main/java/edu/slapoguzov/emodetect/core/ClassExtensions.kt
+++ core/src/main/java/edu/slapoguzov/emodetect/core/ClassExtensions.kt
@@ -1,9 +1,9 @@
 package edu.slapoguzov.emodetect.core
 
-fun <T> Class<T>.readResource(resourceName: String): String {
+fun <T> Class<T>.readResource(ResourceName: String): String {
     return this.classLoader.getResourceAsStream(resourceName).reader().readText()
 }
-
 fun <T> Class<T>.readResourceByLines(resourceName: String): List<String> {
     return this.classLoader.getResourceAsStream(resourceName).reader().readLines()
 }
+
